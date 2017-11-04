fb4d123ca1e663167ead505d8e18205809f842b9
Manvendra Singh <manvendra0310@gmail.com>
2017-11-04 18:40:47 +0530

    bot.py: Major refactoring Took some time out to fix small error, did some major re-factoring - To aid better debugging, append time infront of every print - Use dict for bool conditions rather than dealing with sep variables, cut down for loops by iterating over this new dict. - Sprinkled some useful comments all around

--- bot.py
+++ bot.py
@@ -10,29 +10,11 @@ from pytz import timezone
 # parser.py
 from parser import parser
 
-# INIT Telegram Bot
-# parse .config file to obtain token and public channel name
-with open('.config', 'r') as f:
-    lines = f.readlines()
-    # collect token from the config file
-    TOKEN = lines[0].split()[2].strip("'")
-    BOT_NAME = lines[1].split()[2].strip("'")
-
-# autheticate bot
-try:
-    bot = telegram.Bot(token=TOKEN)
-except:
-    print('Invalid Token, update .config and try again')
-    exit(1)
-
-# sample message
-# bot.sendMessage(chat_id=BOT_NAME, text="hello")
-
-# bool variables init 
-DONE, DONE_DH1, DONE_DH2 = (False,) * 3
-
-print(TOKEN, BOT_NAME)
-print(bot.getMe())
+# Helpful for debug
+def _print(*args):
+    time = datetime.datetime.now().time().isoformat()[:8]
+    print("[{}]:".format(time) , end=' ')
+    print(" ".join(map(str, args)))
 
 
 def prettify_before_sending(data, mess_name):
@@ -41,13 +23,12 @@ def prettify_before_sending(data, mess_name):
     
     meal_in_day = ['Breakfast', 'Lunch', 'Dinner'] 
     for meal_name in meal_in_day:
-        print ('\n')
+        _print ('\n')
         text += '_'+meal_name+':_ \n '
         for item in data[meal_name]:
             text += '- '+item+'\n '
         text += '\n'
 
-    # print (text)
     return text
 
 
@@ -56,53 +37,61 @@ def send_to_channel(data, mess_name):
     text = prettify_before_sending(data, mess_name)
     
     # send to the channel
-    bot.sendMessage(chat_id=BOT_NAME, 
+    bot.sendMessage(chat_id=CHANNEL_NAME, 
                     text=text, 
                     parse_mode='Markdown')
 
 
 def main():
     
-    # use global bools
-    global DONE, DONE_DH1, DONE_DH2 
-    
+    # Parse .config file to obtain TOKEN and CHANNEL NAME
+    with open('.config', 'r') as f:
+        lines = f.readlines()
+        # collect token from the config file
+        TOKEN = lines[0].split()[2].strip("'")
+        CHANNEL_NAME = lines[1].split()[2].strip("'")
+
+    # AUTHETICATION
+    try:
+        bot = telegram.Bot(token=TOKEN)
+    except:
+        _print('Invalid Token, update .config and try again')
+        exit(1)
+
+    # Sample Message
+    # bot.sendMessage(chat_id=CHANNEL_NAME, text="hello")
+    _print(TOKEN, CHANNEL_NAME)
+    _print(bot.getMe())
+
+    # bool variables init 
+    DONE = {"DH1": False, "DH2": False}
+
+    # so-called event loop
     while True:
+
         # get today's datetime, IST timezone 
         ist_timezone = timezone('Asia/Kolkata')
         current_time = datetime.datetime.now(ist_timezone)
-        print("current_time in IST: {}".format(current_time))
+        _print("Time in IST: {}".format(current_time))
 
         # fetch data dict from website
-        data = parser()
-        print("fetch data complete..")
-        if data['DH1']['response'] is True or data['DH2']['response'] is True:
-        	print ('partial/complete menu fetch successful from the website')
+        data = parser() 
+        _print("Date fetch complete.")
 
-        if not DONE_DH1:
-            print(data['DH1']['response'])
-            # if fetched data actually contains menu
-            # and if the date of menu is same as today
-            if data['DH1']['response'] is True and current_time.month == data['DH1']['date'].month and current_time.day == data['DH1']['date'].day:
-            
-                # send message through bot and be done with it
-                print ('sending DH1 menu channel')
-                send_to_channel(data['DH1'], 'DH1')
-                DONE_DH1 = True
+        for key, val in DONE.items():
+            if not DONE[key]:
+                _print(data[key]['response'])
+                # fetch checking logic 
+                if data[key]['response'] is True and current_time.month == data[key]['date'].month and current_time.day == data[key]['date'].day:
+                    
+                    # send menu through bot
+                    _print ('Sending {} menu to channel'.format(key))
+                    send_to_channel(data[key], key)
+                    DONE[key] = True
 
-        if not DONE_DH2:
-            print(data['DH2']['response'])
-            if data['DH2']['response'] is True and current_time.month == data['DH2']['date'].month and current_time.day == data['DH2']['date'].day:
-
-                # send message through bot and be done with it
-                print ('sending DH2 menu to channel')
-                send_to_channel(data['DH2'], 'DH2')
-                DONE_DH2 = True
-
-        # if done for the day ?
-        if DONE_DH1 and DONE_DH2:
-
-            print("ready to sleep now till next day begins")
-            DONE = True
+        # Done for the day?
+        if DONE["DH1"] and DONE["DH2"]:
+            _print("Ready to sleep now until next day 00:01")
 
             # now sleep till 12 am of next day
             current_time = datetime.datetime.now(ist_timezone)
@@ -114,21 +103,36 @@ def main():
 
             # sleep for time delta between these two dates
             time.sleep((wake_up_time - current_time).total_seconds())
+            
             # reinit the variables, cuz new day 
-            print("woke back up")
-            DONE, DONE_DH1, DONE_DH2 = (False,) * 3
+            _print("Woke up. Time to get back to work.")
+            DONE["DH1"] = DONE["DH2"] = False
 
+        # We will get into this else loop when
+        # either one or none of the menu is successfully fetched and posted. 
+        # so sleep only for an hour before checking again. 
+        # Sleep is for rate limiting the hits to actual mess menu website.
         else:
+            # refresh current time
+            current_time = datetime.datetime.now(ist_timezone) 
             # sleep for an hour and then try again
-            print('will try to fetch again in an hour')
+            _print('Sleeping for one hour.')
             time.sleep(60 * 60)
-            # sleep for half an hour and then try again to fetch the menu
-            print('woke up from 1 hr sleep')
+
+            wokeup_time = datetime.datetime.now(ist_timezone)
+
+            # This means you slept on previous day 
+            # and woke up on next day, thus, refresh the DONE dict. 
+            # Fixes edge case when only one mess menu has been posted. 
+            if wokeup_time.date() > current_time.date():
+                DONE["DH1"] = DONE["DH2"] = False
+
+            _print('Woke up!')
 
 if __name__ == '__main__':
-	# don't publish menu for today if cli arguement exists
+    # don't publish menu for today if cli arguement exists
     # not specifiying any specific keyword atm 
     if len(sys.argv) > 1: 
-    	DONE_DH2 = DONE_DH1 = True 
+        DONE["DH1"] = DONE["DH2"] = True
 
     main()
